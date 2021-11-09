b2a8c678cc5f176886ecd6eda2c2abe3110379d0
Coder-Ling <3401506758@qq.com>
2021-11-09 18:06:42 +0800

    新增SentenceService,提供通过deviceId及时间区间查询的接口

---
+++ src/main/java/com/ahpu/motion/service/SentenceService.java
@@ -0,0 +1,15 @@
+package com.ahpu.motion.service;
+
+import com.ahpu.motion.bean.Sentence;
+import com.ahpu.motion.mapper.SentenceMapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface SentenceService extends IService<Sentence> {
+
+    ArrayList<Sentence> selectSentenceBytimeSection(Integer deviceId, String startTime, String endTime);
+
+}
