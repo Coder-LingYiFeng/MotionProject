ae5f478ca3419a5e65ed33ebd31d8a6dc93467ce
Coder-Ling <3401506758@qq.com>
2021-11-09 13:47:51 +0800

    词云数据的controller

--- src/main/java/com/ahpu/motion/controller/WordCloutController.java
+++ src/main/java/com/ahpu/motion/controller/WordCloutController.java
@@ -1,9 +1,13 @@
 package com.ahpu.motion.controller;
 
+import com.ahpu.motion.bean.Device;
 import com.ahpu.motion.utils.ParticipleUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class WordCloutController {
@@ -11,8 +15,10 @@ public class WordCloutController {
     ParticipleUtil participleUtil;
 
     @PostMapping("/getWordCloud")
-    public String getWordCloud(){
-        System.out.println(participleUtil.getParticipleRes("今天真开心！"));
-        return "0";
+    public Map<String, Integer> getWordCloud(Device device){
+        Integer createUserId = device.getCreateUserId();
+
+        return participleUtil.getParticipleRes("今天真开心！");
+
     }
 }
