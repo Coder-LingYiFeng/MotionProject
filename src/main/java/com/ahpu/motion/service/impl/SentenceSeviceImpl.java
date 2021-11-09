65b05a6191c818cb7947240d890e6916c4aed84c
Coder-Ling <3401506758@qq.com>
2021-11-09 18:09:09 +0800

    新增SentenceServiceImpl,实现SentenceServiceImpl

---
+++ src/main/java/com/ahpu/motion/service/impl/SentenceSeviceImpl.java
@@ -0,0 +1,22 @@
+package com.ahpu.motion.service.impl;
+
+import com.ahpu.motion.bean.Sentence;
+import com.ahpu.motion.mapper.SentenceMapper;
+import com.ahpu.motion.service.SentenceService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class SentenceSeviceImpl extends ServiceImpl<SentenceMapper, Sentence> implements SentenceService  {
+
+    @Autowired
+    SentenceMapper sentenceMapper;
+
+    @Override
+    public ArrayList<Sentence> selectSentenceBytimeSection(Integer deviceId, String startTime, String endTime) {
+        return sentenceMapper.selectSentenceBytimeSection(deviceId,startTime,endTime);
+    }
+}
