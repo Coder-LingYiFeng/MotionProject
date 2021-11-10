4257d3a5bcb6dbbbfaeb25a16a7dfe09a5bc7e95
Coder-Ling <3401506758@qq.com>
2021-11-10 19:24:39 +0800

    增加了多个用于保存情感结果的字段

--- src/main/java/com/ahpu/motion/bean/Sentence.java
+++ src/main/java/com/ahpu/motion/bean/Sentence.java
@@ -9,12 +9,28 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class Sentence {
     @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
-    private String deviceId;
+    private Integer deviceId;
     private String sentence;
     private Date time;
+    private Integer sentiment;
+    private Double confidence;
+    private Double positiveProb;
+
+    public Sentence(Integer deviceId, String sentence, Integer sentiment, Double confidence, Double positiveProb, Double negativeProb) {
+        this.deviceId = deviceId;
+        this.sentence = sentence;
+        this.sentiment = sentiment;
+        this.confidence = confidence;
+        this.positiveProb = positiveProb;
+        this.negativeProb = negativeProb;
+    }
+
+    private Double negativeProb;
+
+
 }
